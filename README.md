78402556aba4de5641298ac8fbdfbd54c7e073f3
Bobby <bellfalasch@gmail.com>
2016-03-31 11:51:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# RSS app for Enonic XP version 6
+
+This Enonic XP application adds XXX to your [Enonic XP](https://github.com/enonic/xp) site.
+
+XXX
+
+## Building and deploying
+
+There are two options. One is to simply download the app [JAR file](http://repo.enonic.com/public/com/enonic/app/rss/1.0.0/rss-1.0.0.jar) and move it to the XP installation's `$XP_HOME/deploy` folder.
+
+Or you can build this app with gradle. First, download the zip file of this repo. Unpack it locally. In the terminal, from the root of the project, type `./gradlew build`. On Windows, just type `gradlew build`. Next, move the JAR file from `build/libs` to your `$XP_HOME/deploy` directory. The SEO Meta Fields app will now be available to add to your websites through the Content Manager admin tool in Enonic XP.
+
+If you are upgrading to a newer version of this app, make sure to remove the old version's JAR file from the `$XP_HOME/deploy` directory.
+
+## How to use this app
+
+XXX
+
+## Releases and Compatibility
+
+| Version        | XP version |
+| ------------- | ------------- |
+| 1.0.0 | 6.4.0 |
+
+## Changelog
+
+### Version 1.0.0
+
+* First release
---
+++ src/main/resources/site/filters/add-metadata.js
@@ -0,0 +1,74 @@
+var libs = {
+    portal: require('/lib/xp/portal'),
+    thymeleaf: require('/lib/xp/thymeleaf'),
+    util: require('/lib/enonic/util/util'),
+    site: require('/lib/site')
+};
+
+var view = resolve('add-metadata.html');
+
+// Format locale into the ISO format that Open Graph wants
+var localeMap = {
+    da: 'da_DK',
+    sv: 'sv_SE',
+    pl: 'pl_PL',
+    no: 'nb_NO',
+    en: 'en_US'
+};
+
+exports.responseFilter = function(req, res) {
+    var site = libs.portal.getSite();
+    var content = libs.portal.getContent();
+    var siteConfig = libs.portal.getSiteConfig();
+
+    var lang = content.language || site.language || 'en';
+    var frontpage = site._path === content._path;
+    var pageTitle = libs.site.getPageTitle(content, site);
+
+    // Concat site title?
+    var titleAppendix = '';
+    if (siteConfig.titleBehaviour) {
+        var separator = siteConfig.titleSeparator || '-';
+        if (!frontpage || !siteConfig.titleFrontpageBehaviour) {
+            titleAppendix = ' ' + separator + ' ' + site.displayName;
+        }
+    }
+
+	 var siteVerification = siteConfig.siteVerification || null;
+
+    var params = {
+        title: pageTitle,
+        description: libs.site.getMetaDescription(content, site),
+        siteName: site.displayName,
+        locale: localeMap[lang] || localeMap.en,
+        type: site._path === content._path ? 'website' : 'article',
+        url: libs.portal.pageUrl({ path: content._path, type: "absolute" }),
+        image: libs.site.getOpenGraphImage(content, siteConfig.seoImage),
+        imageWidth: 1200, // Twice of 600x315, for retina
+        imageHeight: 630,
+        blockRobots: siteConfig.blockRobots || libs.site.getBlockRobots(content),
+		  siteVerification: siteVerification
+    };
+
+	var metadata = libs.thymeleaf.render(view, params);
+
+    res.pageContributions.headEnd = libs.util.data.forceArray(res.pageContributions.headEnd);
+    res.pageContributions.headEnd.push(metadata);
+
+    if (res.body) {
+        // Can we find a title in the html? Use that instead of adding our own title
+        var hasIndex = res.body.indexOf('<title>') > -1;
+        var title = '<title>' + pageTitle + titleAppendix + '</title>';
+        if (hasIndex) {
+            res.body = res.body.replace(/(<title>)(.*?)(<\/title>)/i, title);
+        } else {
+            res.pageContributions.headEnd.push(title);
+        }
+    }
+
+    if (req.params.debug === 'true') {
+        res.applyFilters = false; // Skip other filters
+    }
+
+    return res;
+};
