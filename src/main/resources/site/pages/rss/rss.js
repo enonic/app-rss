505c1a1a358041881f9182ab27fa601523427581
Bobby <bellfalasch@gmail.com>
2017-03-27 08:23:37 +0200

    Fix for xslt failures

--- src/main/resources/site/pages/rss/rss.js
+++ src/main/resources/site/pages/rss/rss.js
@@ -281,8 +281,15 @@ exports.get = function(req) {
 // 	libs.util.log(params);
 //		return;
 
+		var body = "";
+		try {
+		    body = libs.xslt.render(view, params);
+		} catch (e) {
+			e.printStackTrace()
+		}
+
 		// Render
-		var body = libs.xslt.render(view, params);
+
 		return {
 			contentType: 'text/xml',
 			body: body
